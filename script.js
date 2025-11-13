3c8e8d34c2d8f1e7d5d96256a4ff61d3b9fddaaa
Elizabeth Bey <ebey04@gmail.com>
2025-11-13 11:31:27 -0500

    feat: add winningPatterns array to Game Controller Section

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let gameBoard = [];
 /* GAME CONTROLLER SECTION */
 
 let gameOver = false;
+
+const winningPatterns = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,4,6], [2,5,8], [3,4,5,] [6,7,8]];
+
 function playerTurn(index) {
 
     Is gameOver true?
