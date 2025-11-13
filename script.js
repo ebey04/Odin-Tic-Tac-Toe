1d4aeb8f079d863cad05fef0a548270a28e56265
Elizabeth Bey <ebey04@gmail.com>
2025-11-13 11:27:39 -0500

    feat: add psuedocode function & variable to Game Controller section

--- script.js
+++ script.js
@@ -4,5 +4,51 @@ let gameBoard = [];
 
 /* GAME CONTROLLER SECTION */
 
+let gameOver = false;
+function playerTurn(index) {
+
+    Is gameOver true?
+
+    Yes → return
+
+    No → continue
+
+    Is board[index] empty?
+
+    No → return
+
+    Yes → place current player's token in board[index]
+
+    Loop through winningPatterns
+
+    If a match →
+
+    gameOver = true
+
+    show winning message
+
+    show reset button
+
+    return
+
+    Check for tie
+
+    If NO empty slots →
+
+    gameOver = true
+
+    show tie message
+
+    show reset button
+
+    return
+
+    No win + no tie
+
+    Switch player
+
+    Return (turn ends)
+
+}
 
 /*DISPLAY CONTROLLER SECTION */
