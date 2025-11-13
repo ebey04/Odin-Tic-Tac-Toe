213fa6966cd40ef0a21376ecef70ef15426c52a8
Elizabeth Bey <ebey04@gmail.com>
2025-11-13 14:37:11 -0500

    fix: add , in winningPatterns []

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let gameOver = false;
 let currentPlayer = playerOne;
 
 
-const winningPatterns = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,4,6], [2,5,8], [3,4,5,] [6,7,8]];
+const winningPatterns = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,4,6], [2,5,8], [3,4,5,], [6,7,8]];
 
 function switchPlayer() {
     currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
