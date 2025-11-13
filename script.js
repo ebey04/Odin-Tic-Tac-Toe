c53969b5c48614b1bd643306a461ff65a7f425bb
Elizabeth Bey <ebey04@gmail.com>
2025-11-13 12:58:06 -0500

    feat: add event listener to all squares

--- script.js
+++ script.js
@@ -70,14 +70,20 @@ function playerTurn(index) {
 
 /*DISPLAY CONTROLLER SECTION */
 
-//Pull in HTML elements
-
 const gameboard = document.getElementById("gameboard");
-const square = document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.square');
 const messageArea = document.getElementById("messages");
 const resetBtn = document.getElementById("resetBtn");
 
 //Event Listeners
+
+square.forEach(cell => {
+    cell.addEventListener('click', () => {
+        let index = cell.dataset.index;
+        playerTurn(index);
+    });
+});
+
 
 //Send index to Game Controller Section 
 
